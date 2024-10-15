17f8c80851a5221a35f944a4fdc7b81c2c91b648
NicolasBasquin <basquinnicolas.bn@gmail.com>
2024-10-15 15:33:10 +0200

    Update FriendQuery.java

--- src/main/java/foo/FriendQuery.java
+++ src/main/java/foo/FriendQuery.java
@@ -23,72 +23,63 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 @WebServlet(name = "FriendQuery", urlPatterns = { "/query" })
 public class FriendQuery extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
 
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-        response.getWriter().print("<h1> Friends Queries </h1>");
+		// 1. Give me the age of "f0"
+		response.getWriter().print("<h2>Give me the age of 'f0'</h2>");
+		try {
+			Entity e = datastore.get(KeyFactory.createKey("Friend", "f0"));
+			response.getWriter().print("<li>Age of f0: " + e.getProperty("age"));
+		} catch (EntityNotFoundException e1) {
+			response.getWriter().print("<li>f0 not found.");
+		}
 
-        // 1. Give me the age of “f0”
-        response.getWriter().print("<h2> Age of 'f0'</h2>");
-        try {
-            Entity f0 = datastore.get(KeyFactory.createKey("Friend", "f0"));
-            response.getWriter().print("<li>Age of 'f0': " + f0.getProperty("age") + "</li>");
-        } catch (EntityNotFoundException e1) {
-            response.getWriter().print("<li>'f0' not found.</li>");
-        }
+		// 2. Give me at most 5 users with age 30
+		response.getWriter().print("<h2>Give me at most 5 users with age 30</h2>");
+		Query ageQuery = new Query("Friend").setFilter(new FilterPredicate("age", FilterOperator.EQUAL, 30));
+		List<Entity> ageResult = datastore.prepare(ageQuery).asList(FetchOptions.Builder.withLimit(5));
+		response.getWriter().print("<li>Users with age 30 (max 5):<br>");
+		for (Entity entity : ageResult) {
+			response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName"));
+		}
 
-        // 2. Give me at most 5 users with age 30
-        response.getWriter().print("<h2> At most 5 users with age 30</h2>");
-        Query q = new Query("Friend").setFilter(new FilterPredicate("age", FilterOperator.EQUAL, 30));
-        PreparedQuery pq = datastore.prepare(q);
-        List<Entity> result = pq.asList(FetchOptions.Builder.withLimit(5));
-        response.getWriter().print("<li>Number of users found: " + result.size() + "</li>");
-        for (Entity entity : result) {
-            response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName")
-                    + " (Age: " + entity.getProperty("age") + ")</li>");
-        }
+		// 3. Give me all users whose name starts with "f14"
+		response.getWriter().print("<h2>Give me all users whose name starts with 'f14'</h2>");
+		Query nameQuery = new Query("Friend").setFilter(new FilterPredicate("firstName", FilterOperator.GREATER_THAN_OR_EQUAL, "f14"))
+				.setFilter(new FilterPredicate("firstName", FilterOperator.LESS_THAN, "f15"));
+		List<Entity> nameResult = datastore.prepare(nameQuery).asList(FetchOptions.Builder.withDefaults());
+		response.getWriter().print("<li>Users with name starting with 'f14':<br>");
+		for (Entity entity : nameResult) {
+			response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName"));
+		}
 
-        // 3. Give me all users whose name starts with “f14”
-        response.getWriter().print("<h2> Users whose first name starts with 'f14'</h2>");
-        q = new Query("Friend")
-                .setFilter(CompositeFilterOperator.and(
-                        new FilterPredicate("firstName", FilterOperator.GREATER_THAN_OR_EQUAL, "f14"),
-                        new FilterPredicate("firstName", FilterOperator.LESS_THAN, "f15")));
-        pq = datastore.prepare(q);
-        result = pq.asList(FetchOptions.Builder.withDefaults());
-        response.getWriter().print("<li>Number of users found: " + result.size() + "</li>");
-        for (Entity entity : result) {
-            response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName")
-                    + "</li>");
-        }
+		// 4. Give me all users that are friends with "f0"
+		response.getWriter().print("<h2>Give me all users that are friends with 'f0'</h2>");
+		Query friendsQuery = new Query("Friend").setFilter(new FilterPredicate("friends", FilterOperator.EQUAL, "f0"));
+		List<Entity> friendsResult = datastore.prepare(friendsQuery).asList(FetchOptions.Builder.withDefaults());
+		response.getWriter().print("<li>Friends of 'f0':<br>");
+		for (Entity entity : friendsResult) {
+			response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName"));
+		}
 
-        // 4. Give me all users that are friends with “f0”
-        response.getWriter().print("<h2> Users that are friends with 'f0'</h2>");
-        q = new Query("Friend").setFilter(new FilterPredicate("friends", FilterOperator.EQUAL, "f0"));
-        pq = datastore.prepare(q);
-        result = pq.asList(FetchOptions.Builder.withDefaults());
-        response.getWriter().print("<li>Number of users found: " + result.size() + "</li>");
-        for (Entity entity : result) {
-            response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName")
-                    + "</li>");
-        }
-
-        // 5. Give me all users that are friends with “f0” and have age = 10
-        response.getWriter().print("<h2> Users that are friends with 'f0' and have age 10</h2>");
-        q = new Query("Friend").setFilter(CompositeFilterOperator.and(
-                new FilterPredicate("friends", FilterOperator.EQUAL, "f0"),
-                new FilterPredicate("age", FilterOperator.EQUAL, 10)));
-        pq = datastore.prepare(q);
-        result = pq.asList(FetchOptions.Builder.withDefaults());
-        response.getWriter().print("<li>Number of users found: " + result.size() + "</li>");
-        for (Entity entity : result) {
-            response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName")
-                    + "</li>");
-        }
-    }
+		// 5. Give me all users that are friends with "f0" with age = 10
+		response.getWriter().print("<h2>Give me all users that are friends with 'f0' with age 10</h2>");
+		Query friendsWithAgeQuery = new Query("Friend")
+				.setFilter(CompositeFilterOperator.and(
+						new FilterPredicate("friends", FilterOperator.EQUAL, "f0"),
+						new FilterPredicate("age", FilterOperator.EQUAL, 10)
+				));
+		List<Entity> friendsWithAgeResult = datastore.prepare(friendsWithAgeQuery).asList(FetchOptions.Builder.withDefaults());
+		response.getWriter().print("<li>Friends of 'f0' with age 10:<br>");
+		for (Entity entity : friendsWithAgeResult) {
+			response.getWriter().print("<li>" + entity.getProperty("firstName") + " " + entity.getProperty("lastName"));
+		}
+	}
 }
+
